174f5ad136fd051c10b4c5250b87733a715faa3f
Rolo <89317868+Roloper@users.noreply.github.com>
2023-02-09 02:11:08 -0500

    r

--- Ejercicio3Glaciar/ejer3.py
+++ Ejercicio3Glaciar/ejer3.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
+
+# Leer la imagen
 from stackImages import StackImagen
 
-#Cargar la imagen
-img = cv2.imread(r"imagenes2\bacteria2.jpg")
+img = cv2.imread(r'imagenes/glaciar.jpg')
 img2 = img.copy()
 # Convertir la imagen a escala de grises
 gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -19,25 +20,17 @@ def getContorno(img):
     cont = 0
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     for cnt in contours:
+        area = cv2.contourArea(cnt)
+        print(area)
         cv2.drawContours(img2,cnt,-1,(255,0,0),3)
-        perimetro = cv2.arcLength(cnt,True)
-        aprox = cv2.approxPolyDP(cnt,0.02*perimetro,True)
-        objcorner=len(aprox)
-        x,y,w,h = cv2.boundingRect(aprox)
-        if objcorner >4:
-            b = "Bacteria"
-            cont=cont+1
-        else:
-            b = "?"
-        cv2.rectangle(img2,(x,y),(x+w,y+h), (0,255,0),2)
-        cv2.putText(img2, b,(x+(w//2)-18, y + (h//2)-18), cv2.FONT_HERSHEY_COMPLEX,0.7,(0,0,0),2)
-    print("En esta imagen hay ",cont," BACTERIAS")
+        cont = cont +area
+    print("El area total del glaciar es: ",cont)
 #Llamamos a la funcion
 getContorno(borde)
 
 #Mostrar las imágenes resultantes creando un objeto de la clase StackImagen
 stack = StackImagen(0.4)
-imagenes = [img,gris,gaus,img2,borde]
+imagenes = [img,gris,gaus,img2]
 
 
 result = stack.stack_images(imagenes)

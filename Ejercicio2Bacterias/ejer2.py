d8ca0637b537642b1950a6f21e133d9471fda3b1
Rolo <89317868+Roloper@users.noreply.github.com>
2023-02-09 01:53:34 -0500

    r

--- Ejercicio2Bacterias/ejer2.py
+++ Ejercicio2Bacterias/ejer2.py
@@ -2,14 +2,9 @@ import cv2
 import numpy as np
 from stackImages import StackImagen
 
-def getContorno(img):
-
-
-
-
 #Cargar la imagen
-img = cv2.imread(r"imagenes2\bacterias.jpg")
-
+img = cv2.imread(r"imagenes2\bacteria.jpg")
+img2 = img.copy()
 # Convertir la imagen a escala de grises
 gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -19,10 +14,35 @@ gaus = cv2.GaussianBlur(gris, (7,7), 0)
 # Aplicar filtro Canny para detectar bordes
 borde= cv2.Canny(gaus,50,50)
 
+#Funcion para obtener area de las figuras que salen en la imagen
+def getContorno(img):
+    cont = 0
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        print(area)
+        cv2.drawContours(img2,cnt,-1,(255,0,0),3)
+        perimetro = cv2.arcLength(cnt,True)
+        print(perimetro)
+        aprox = cv2.approxPolyDP(cnt,0.02*perimetro,True)
+        print(len(aprox))
+        objcorner=len(aprox)
+        x,y,w,h = cv2.boundingRect(aprox)
+        if objcorner >4:
+            b = "Bacteria"
+            cont=cont+1
+        else:
+            b = "?"
+        cv2.rectangle(img2,(x,y),(x+w,y+h), (0,255,0),2)
+        cv2.putText(img2, b,(x+(w//2)-18, y + (h//2)-18), cv2.FONT_HERSHEY_COMPLEX,0.7,(0,0,0),2)
+        print("En esta imagen hay ",cont," BACTERIAS")
+#Llamamos a la funcion
+getContorno(borde)
 
-stack = StackImagen(0.8)
+#Mostrar las imágenes resultantes creando un objeto de la clase StackImagen
+stack = StackImagen(0.4)
+imagenes = [img,gris,gaus,img2,borde]
 
-imagenes = [img,gris,gaus,borde,]
 
 result = stack.stack_images(imagenes)
 cv2.imshow("Resultado",result)
